486a9927e1374ba8e8a433d83136effa006854a9
Haocheng Yang <haocheng.yang@appfolio.com>
2020-06-02 15:37:59 -0700

    hy - add image new&show view and image new&show&create action

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,30 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  # def index
+  #   @images = Image.order(created_at: :desc)
+  # end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:notice] = 'Image was saved successfully'
+      redirect_to @image
+    else
+      flash[:alert] = 'Error found!'
+      render 'new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:name, :url)
+  end
+end
