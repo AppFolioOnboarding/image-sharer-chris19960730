a2b2623640765ac66e6c8c70372373c6ba21f665
Haocheng Yang <haocheng.yang@appfolio.com>
2020-06-02 15:33:24 -0700

    hy - add Image model and add validation method for image url

---
+++ app/models/image.rb
@@ -0,0 +1,42 @@
+class Image < ApplicationRecord
+  # Validation for image columns.
+  validates :name, presence: true
+  validates :url, presence: true
+  validate :image_url_valid?
+
+  private
+
+  # Define method to check if the Image URL is valid.
+  def check_with_http(url)
+    url = URI.parse(url)
+    Net::HTTP.start(url.host, url.port) do |http|
+      return http.head(url.request_uri)['Content-Type'].start_with? 'image'
+    end
+  rescue StandardError => e
+    print e.message
+    nil
+  end
+
+  def check_with_https(url)
+    url = URI.parse(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == 'https')
+
+    http.start do |https|
+      return https.head(url.request_uri)['Content-Type'].start_with? 'image'
+    end
+  rescue StandardError => e
+    print e.message
+    nil
+  end
+
+  def image_url_valid?
+    return false if url.blank?
+
+    if url =~ /^https/
+      errors.add(:url, 'invalid image url') unless check_with_https(url)
+    else
+      errors.add(:url, 'invalid image url') unless check_with_http(url)
+    end
+  end
+end
