4dc063272671320c4ffa8beaac094457deae164a
Chris <haocheng.yang@appfolio.com>
2020-06-03 11:42:31 -0700

    hy - Adjust code style

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ require_relative 'application'
 Rails.application.initialize!
 
 # Disable auto generate field_with_errors div.
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   html_tag.html_safe
 end
